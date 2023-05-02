13418544ed7811d617bd5ccdf0830f7b4960be2b
Olivier <olivier.ledc@gmail.com>
2023-05-02 11:05:43 +0200

    create location store

---
+++ resources/js/stores/locationStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+
+export const useLocationStore = defineStore({
+    id: 'locationStore',
+    state: () => ({
+
+    }),
+    getters: {
+
+    },
+    actions: {
+        
+    }
+})
