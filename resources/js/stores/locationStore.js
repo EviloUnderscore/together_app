e3bb07c490dd29049a8ae76d9c179988ad20420d
Olivier <olivier.ledc@gmail.com>
2023-05-02 11:18:41 +0200

    Add fetchLocation method

--- resources/js/stores/locationStore.js
+++ resources/js/stores/locationStore.js
@@ -3,12 +3,23 @@ import { defineStore } from "pinia";
 export const useLocationStore = defineStore({
     id: 'locationStore',
     state: () => ({
-
+        userLatiture: null,
+        userLongitude: null,
     }),
     getters: {
 
     },
     actions: {
-        
+        async fetchLocation() {
+            try {
+              const position = await new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject)
+              })
+              this.userLatitude = position.coords.latitude
+              this.userLongitude = position.coords.longitude
+            } catch (error) {
+              console.error(error)
+            }
+          }
     }
 })
