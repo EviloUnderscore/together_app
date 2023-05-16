430243161f9b0c694c4c3afbc2d1e3378aeef9fe
Olivier <olivier.ledc@gmail.com>
2023-05-16 11:08:12 +0200

    change logout button css

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -58,7 +58,7 @@ const logout = () => {
                     </svg>
                 </a>
                 <form @submit.prevent="logout" class="w-1/4 flex justify-center items-center text-center">
-                    <button type="submit">
+                    <button type="submit" class="w-full h-full flex justify-center items-center text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                         </svg>
